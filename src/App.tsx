15bc09f88627d442efb6e1bd9d6c0c7e044c8edc
AlexKhabibov <alex.lv.u@gmail.com>
2025-10-22 17:33:10 +0300

    PRJ-5: starting project - structure done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Profile from './components/Profile'
+import Search from './components/Search'
 
 function App() {
 
-
   return (
-    <>
-
-    </>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <Profile />
+      <Search />
+      <Content />
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/data/data.tsx
@@ -0,0 +1,39 @@
+import type { Product } from "./types";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: "Кроссовки Nike",
+        price: 120,
+        description: "Комфортные спортивные кроссовки",
+        image: "/assets/nike.png"
+    },
+    {
+        id: 2,
+        name: "Футболка Adidas",
+        price: 35,
+        description: "Стильная футболка",
+        image: "/assets/adidas.png"
+    },
+    {
+        id: 3,
+        name: "Рюкзак Puma",
+        price: 60,
+        description: "Удобный рюкзак для спорта и прогулок",
+        image: "/assets/puma.png"
+    },
+    {
+        id: 4,
+        name: "Бейсболка New Era",
+        price: 25,
+        description: "Классическая бейсболка с логотипом",
+        image: "/assets/newera.png"
+    },
+    {
+        id: 5,
+        name: "Спортивные штаны Under Armour",
+        price: 50,
+        description: "Легкие и дышащие штаны для тренировок",
+        image: "/assets/underarmour.png"
+    }
+];
