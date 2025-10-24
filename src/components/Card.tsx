4f0335a7d6a34c686f32dd21034843cc1c69699c
AlexKhabibov <alex.lv.u@gmail.com>
2025-10-24 18:57:54 +0300

    PRJ-9, PRJ-10: Card and CardList comps done

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,30 @@
-function Card() {
+import type { IProduct } from "../data/types";
+
+function Card({ name, description, price, image }: IProduct) {
+
+    const handleRedirect = () => {
+
+    }
+
     return (
-        <>
-            card
-        </>
+        <div
+            className="card"
+            style={{ width: 220, padding: 10, border: "1px solid #ccc", borderRadius: 8 }}
+        >
+            {/* зона клика */}
+            <div onClick={handleRedirect} style={{ cursor: "pointer" }}>
+                {/* сделать редирект */}
+                <img
+                    src={image}
+                    alt={name}
+                    style={{ width: "100%", height: 150, objectFit: "contain", borderRadius: 4 }}
+                />
+                <h3>{name}</h3>
+                <p style={{ minHeight: 40 }}>{description}</p>
+                <p>{price} ₽</p>
+            </div>
+            {/* добавить кнопки "лайк" и "в корзину" */}
+        </div>
     );
 }
 
